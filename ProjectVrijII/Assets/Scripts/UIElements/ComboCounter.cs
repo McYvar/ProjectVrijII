f0dc0bdc84e447f452b5b4b0a8b47d1a39e0a0b9
chan92 <angell0vee@hotmail.com>
2023-05-16 16:47:35 +0200

    Added: UI Combo counter

---
+++ ProjectVrijII/Assets/Scripts/UIElements/ComboCounter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.Rendering;
+
+public class ComboCounter : CombatBase {
+	[Header("CanvasElements")]
+	[SerializeField]
+	private TMP_Text comboText;
+	[SerializeField]
+	private CanvasGroup counterObject;
+
+	[Header("CounterText")]
+	[SerializeField]
+	private int textSize = 20;
+	[SerializeField]
+	private string textString = "Hits!!";
+
+	[Header("CounterNumbers")]
+	[SerializeField]
+	private TextEffect[] counterEffects;
+
+	private int effectId = -1;
+	private int comboCounter;
+
+	private void Start() {
+		ResetCombo();
+	}
+
+	//Call this to reset and remove the combo
+	public void ResetCombo() {
+		comboCounter = 0;
+		effectId = -1;
+		counterObject.alpha = 0;
+	}
+
+	//Call this to increase the combo and make it visible
+	public void IncreaseCombo() {
+		comboCounter++;
+		SetComboText();
+		counterObject.alpha = 1;
+	}
+
+	//sets the text and checks for change in effects
+	private void SetComboText() {	
+		comboText.text = $"{comboCounter}<size={textSize}>{textString}</size>";
+		
+		if(effectId < counterEffects.Length - 1) {
+			if(comboCounter >= counterEffects[effectId + 1].StartEffect) {
+				effectId++;
+				SetEffects();
+			}
+		}
+	}
+
+	private void SetEffects() {
+		comboText.fontSize = counterEffects[effectId].TextSize;
+		comboText.fontStyle = counterEffects[effectId].TextInBold ? FontStyles.Bold : FontStyles.Normal;
+		comboText.color = counterEffects[effectId].TextColor;
+		comboText.colorGradientPreset = counterEffects[effectId].Gradient;
+	}
+}
