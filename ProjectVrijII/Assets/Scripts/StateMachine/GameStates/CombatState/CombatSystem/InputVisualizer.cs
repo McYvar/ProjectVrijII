16c69e751355962fe641af53e1a1f53fa7b23df4
chan92 <angell0vee@hotmail.com>
2023-05-16 23:30:09 +0200

    Added: basic input visualizer

--- ProjectVrijII/Assets/Scripts/StateMachine/GameStates/CombatState/CombatSystem/InputVisualizer.cs
+++ ProjectVrijII/Assets/Scripts/StateMachine/GameStates/CombatState/CombatSystem/InputVisualizer.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InputVisualizer : CombatBase
-{
+public class InputVisualizer : CombatBase {
+	[SerializeField]
+	private CanvasGroup inputInfoObject;
 
+	[SerializeField]
+	private RectTransform inputInfo;
+
+	[SerializeField]
+	private float distance = 30;
+
+	private PlayerInput playerInput;
+
+	private void Start() {
+		playerInput = FindObjectOfType<PlayerInput>();
+	}
+
+	public override void OnUpdate() {
+		SetInputPoint();
+	}
+
+	//can be used to disable and re enable (is enabled by standard)
+	public void EnableInputInfo(bool enable) {
+		inputInfoObject.alpha = enable ? 1 : 0;
+	}
+
+	private void SetInputPoint() {
+		Vector2 newpos = Vector2.zero;
+		newpos = playerInput.leftDirection;
+
+		inputInfo.anchoredPosition = newpos * distance;
+	}
 }
