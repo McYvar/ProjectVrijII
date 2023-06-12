e09d2008d28dd35875e454cee2ed1919e74f4ca5
Chantal <c_voort1992@hotmail.com>
2023-06-12 11:39:50 +0200

    Updated hp bar

--- ProjectVrijII/Assets/Scripts/UIHealthBar.cs
+++ ProjectVrijII/Assets/Scripts/UIHealthBar.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIHealthBar : MonoBehaviour {
 
     private Health health;
     [SerializeField]
     private Image healthBarFilling;
+    [SerializeField]
+    private TMP_Text healthText;
+    [SerializeField]
+    private float fullHealthPosition;
+    [SerializeField]
+    private float emptyHealthPosition;
+
+    private Vector2 healthStartPosition;
 
     private void OnDisable() {
         health.OnHealthChanged -= HealthChanged;
@@ -16,12 +25,19 @@ public class UIHealthBar : MonoBehaviour {
 
     public void InitializeUIHealthbar(Health health) {
         this.health = health;
+        healthStartPosition = healthBarFilling.rectTransform.localPosition;
         this.health.OnHealthChanged += HealthChanged;
     }
 
     //Called when hp changes
     private void HealthChanged(float newHealth, float maxHealth) {
-        float healthPercent = newHealth / maxHealth;
-        healthBarFilling.fillAmount = healthPercent;
+         float healthPercent = newHealth / maxHealth;
+        //  healthBarFilling.fillAmount = healthPercent;
+
+        Vector2 healthFillPosition = healthStartPosition;
+        healthFillPosition.x = Mathf.Lerp(emptyHealthPosition, fullHealthPosition, healthPercent);
+        healthBarFilling.rectTransform.localPosition = healthFillPosition;
+
+        if (healthText) healthText.text = $"{Mathf.Ceil(newHealth)}";
     }
 }
