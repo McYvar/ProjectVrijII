6fecc72e194440f6524f8284c6a77a4148c2e2cd
McYvar <yvarftoorop@hotmail.com>
2023-06-15 01:20:48 +0200

    Ui element reposition

--- ProjectVrijII/Assets/Scripts/FModScripts/FModEventCaller.cs
+++ ProjectVrijII/Assets/Scripts/FModScripts/FModEventCaller.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
-using static UnityEngine.UIElements.UxmlAttributeDescription;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager.UI;
 
 public class FModEventCaller : MonoBehaviour
 {
